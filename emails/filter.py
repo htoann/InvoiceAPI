90b18e05855ce0e36a21c31d7a046c448d8fd537
Toan Tran <huutrantoan@gmail.com>
2024-08-03 16:03:07 +0700

    Just commit

--- emails/filter.py
+++ emails/filter.py
@@ -14,23 +14,7 @@ class MailAccountFilterSet(django_filter.FilterSet):
         fields = ['name', 'email']
 
 class MailFilterSet(django_filter.FilterSet):
-
-    # email = django_filters.DateFilter(method='user__email', label='Email', input_formats=["%d-%m-%Y"])
-    # date_to = django_filters.DateFilter(method='filter_date_to', label='Date To', input_formats=["%d-%m-%Y"])
     email = django_filters.CharFilter(field_name='mail_account__email', lookup_expr='icontains')
-
-    # def filter_date_from(self, queryset, name, value):
-    #     if value:
-    #         return queryset.filter(ntao__gte=value)
-        
-    #     return queryset
-
-    # def filter_date_to(self, queryset, name, value):
-    #     if value:
-    #         value += timedelta(days=1)
-    #         queryset = queryset.filter(ntao__lt=value)
-        
-    #     return queryset
 
     class Meta:
         model = MailInbox
